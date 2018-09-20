9b970d3d8052081d59628129b91714cc596d9217
pedro.pacheco.a@gmail.com <pedro.pacheco.a@gmail.com>
2018-09-19 22:46:42 -0300

    Layout inicial

--- telaChat/App.js
+++ telaChat/App.js
@@ -1,49 +1,46 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import { View, Platform, StyleSheet, FlatList, ImageBackground } from 'react-native';
+import MsgItem from './src/MsgItem';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+export default class PrimeiroProjeto extends Component {
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+	constructor(props) {
+		super(props);
+		this.state = {
+			chat:[
+				{key:'1', nome:'Pedro', msg:'Oi', m:true},
+				{key:'2', nome:'Fernanda', msg:'Opa, tudo bem?', m:false},
+				{key:'3', nome:'Fernanda', msg:'Como vai a vida?', m:false},
+				{key:'4', nome:'Pedro', msg:'Vai muito bem, graças a Deus. E você? Eu soube que você está bem legal!', m:true},
+				{key:'5', nome:'Fernanda', msg:'Também, muito legal!', m:false},
+				{key:'6', nome:'Pedro', msg:'Maravilha!', m:true},
+				{key:'7', nome:'Pedro', msg:'Grande abraço!', m:true},
+				{key:'8', nome:'Fernanda', msg:'Bjos!', m:false},
+				{key:'9', nome:'Fernanda', msg:'Até mais!', m:false}
+			]
+		};
+	}
+	render() {
+		return (
+			<View style={styles.container}>
+				<ImageBackground source={require("./assets/images/bg.jpg")} style={styles.chat}>
+					<FlatList
+						data={this.state.chat}
+						renderItem={({item})=> <MsgItem data={item} />}
+					/>
+				</ImageBackground>
+			</View>
+		);
+	}
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container:{
+		flex:1,
+		marginTop:(Platform.OS=='ios') ? 20 : 0
+	},
+	chat:{
+		flex:1
+	}
 });
